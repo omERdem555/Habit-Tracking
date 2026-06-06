73991f0565267a1ee221fc9214fc760099efe96a
omERdem555 <omererdem555@outlook.com>
2026-06-06 22:02:06 +0300

    Mobil bildiriminde logo sorunu

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,11 +39,11 @@ const RUNTIME_CACHE = `habit-tracker-runtime-${CACHE_VERSION}`;
 
 const PRECACHE_URLS = [
   '/',
-  '/manifest.json',
-  '/icon72.png',
-  '/icon96.png',
-  '/icon192.png',
-  '/icon512.png',
+  './manifest.json',
+  './icon72.png',
+  './icon96.png',
+  './icon192.png',
+  './icon512.png',
 ];
 
 self.addEventListener('install', (event) => {
