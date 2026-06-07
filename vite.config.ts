2bb2ad3c3b1edec7f764cab15dcaa78486934506
omERdem555 <omererdem555@outlook.com>
2026-06-07 15:19:10 +0300

    Firebase hataları alıyorum

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: './',
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true
